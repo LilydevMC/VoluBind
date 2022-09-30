a30eb3de2b8ce8d7c536f9358f8248f36f71b2b6
Jade Nash <jade.nashh18@gmail.com>
2022-09-30 13:37:17 -0500

    more setup + set ModInitializer to ClientModInitializer

--- src/main/java/com/lilydev/volubind/VoluBind.java
+++ src/main/java/com/lilydev/volubind/VoluBind.java
@@ -1,18 +1,18 @@
 package com.lilydev.volubind;
 
 import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class VoluBind implements ModInitializer {
+public class VoluBind implements ClientModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("VoluBind");
 
 	@Override
-	public void onInitialize(ModContainer mod) {
+	public void onInitializeClient(ModContainer mod) {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
 	}
 }
