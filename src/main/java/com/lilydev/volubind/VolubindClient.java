bd8532fc6016846c04962569d0ebfb69c2e4140e
Jade <jade@lilydev.com>
2023-10-01 23:12:00 -0500

    Add translation/config option for all volume types (non-functional)

--- src/main/java/com/lilydev/volubind/VolubindClient.java
+++ src/main/java/com/lilydev/volubind/VolubindClient.java
@@ -1,9 +1,8 @@
 package com.lilydev.volubind;
 
+import com.lilydev.volubind.config.ConfigConsumers;
 import com.lilydev.volubind.config.VolubindConfig;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.sound.SoundCategory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,21 +19,7 @@ public class VolubindClient implements ClientModInitializer {
     public void onInitializeClient() {
         LOGGER.info("test! " + MOD_NAME);
 
-        //SoundOptionsScreen
-
-        //LOGGER.info(SoundCategory.);
-
-        //CONFIG.subscribeToMasterVolTest();
-
-        MinecraftClient client = MinecraftClient.getInstance();
-
-        CONFIG.subscribeToMasterVolume(vol -> {
-            LOGGER.info("volume: " + client.options.getSoundVolumeOption(SoundCategory.MASTER));
-
-            if (!CONFIG.masterToggled()) {
-                client.options.getSoundVolumeOption(SoundCategory.MASTER).setValue(vol.doubleValue() / 100);
-            }
-        });
+        ConfigConsumers.register();
 
     }
 }
--- src/main/java/com/lilydev/volubind/config/ConfigModel.java
+++ src/main/java/com/lilydev/volubind/config/ConfigModel.java
@@ -2,24 +2,78 @@ package com.lilydev.volubind.config;
 
 import io.wispforest.owo.config.annotation.*;
 import com.lilydev.volubind.VolubindClient;
-import net.minecraft.client.MinecraftClient;
 
+@SuppressWarnings("unused")
 @Modmenu(modId = VolubindClient.MOD_ID)
 @Config(name = "volubind", wrapperName = "VolubindConfig")
 public class ConfigModel {
-    @SectionHeader("volumeSection")
-
     @Hook
     @RangeConstraint(min = 0, max = 100)
     public int masterVolume = 100;
-
-
-    @SectionHeader("volumeToggledSection")
-
     @Hook
     @RangeConstraint(min = 0, max = 100)
-    public int masterVolumeToggled = 100;
+    public int musicVolume = 100;
+    @Hook
+    @RangeConstraint(min = 0, max = 100)
+    public int musicBlockVolume = 100;
+    @Hook
+    @RangeConstraint(min = 0, max = 100)
+    public int weatherVolume = 100;
+    @Hook
+    @RangeConstraint(min = 0, max = 100)
+    public int blockVolume = 100;
+    @Hook
+    @RangeConstraint(min = 0, max = 100)
+    public int hostileVolume = 100;
+    @Hook
+    @RangeConstraint(min = 0, max = 100)
+    public int friendlyVolume = 100;
+    @Hook
+    @RangeConstraint(min = 0, max = 100)
+    public int playerVolume = 100;
+    @Hook
+    @RangeConstraint(min = 0, max = 100)
+    public int ambientVolume = 100;
+    @Hook
+    @RangeConstraint(min = 0, max = 100)
+    public int voiceVolume = 100;
+
+    @Nest
+    public VolumeToggled volumeToggled = new VolumeToggled();
 
     @ExcludeFromScreen
-    public boolean masterToggled;
+    public boolean masterToggled = false;
+
+    public static class VolumeToggled {
+        @Hook
+        @RangeConstraint(min = 0, max = 100)
+        public int masterVolumeToggled = 0;
+        @Hook
+        @RangeConstraint(min = 0, max = 100)
+        public int musicVolumeToggled = 100;
+        @Hook
+        @RangeConstraint(min = 0, max = 100)
+        public int musicBlockVolumeToggled = 100;
+        @Hook
+        @RangeConstraint(min = 0, max = 100)
+        public int weatherVolumeToggled = 100;
+        @Hook
+        @RangeConstraint(min = 0, max = 100)
+        public int blockVolumeToggled = 100;
+        @Hook
+        @RangeConstraint(min = 0, max = 100)
+        public int hostileVolumeToggled = 100;
+        @Hook
+        @RangeConstraint(min = 0, max = 100)
+        public int friendlyVolumeToggled = 100;
+        @Hook
+        @RangeConstraint(min = 0, max = 100)
+        public int playerVolumeToggled = 100;
+        @Hook
+        @RangeConstraint(min = 0, max = 100)
+        public int ambientVolumeToggled = 100;
+        @Hook
+        @RangeConstraint(min = 0, max = 100)
+        public int voiceVolumeToggled = 100;
+    }
 }
